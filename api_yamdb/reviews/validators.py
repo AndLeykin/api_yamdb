862ce733262edc106ec41f4ebc0db7619d2ebf6e
Андрей Лейкин <andleykin@yandex.ru>
2023-06-04 00:15:30 +0300

    validator edit

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 def validate_year(value):
     year = dt.date.today().year
-    if value > year or year < 0:
+    if value > year or value < 0:
         raise ValidationError(
             'Необходимо проверить год публикации произведения!'
         )
