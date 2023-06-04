d039555fedd6da31aa035be0b4ec333a16cb3132
Алексей Вишняков <alexvish8@yandex.ru>
2023-06-04 19:59:26 +0300

    Разраб 2 замечания исправлены тесты все проходит

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -45,9 +45,9 @@ class TitleViewSet(viewsets.ModelViewSet):
     filterset_class = GenreFilter
 
     def get_serializer_class(self):
-        if self.action in ('create', 'update'):
-            return TitleReadSerializer
-        return TitleWriteSerializer
+        if self.action in ('create', 'update', 'partial_update'):
+            return TitleWriteSerializer
+        return TitleReadSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
